c2c24c60c331ab6fa3167e2a0512701446d45db7
Abhishek Ranjan <56349327+abhi2103ar7@users.noreply.github.com>
2020-02-25 11:10:12 +0530

    Add files via upload

---
+++ Project/includes/header.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-dark bg-nav">
+    <div class="row">
+        <div class="col-4">
+            <img src="https://www.flaticon.com/premium-icon/icons/svg/1820/1820305.svg" style="width: 150px;height: 150px">
+        </div>
+        <div class="col-md-8 padding-30">
+            <h1 style="text-align: center;margin-top: 50px"><b>Zomato</b></h1>
+
+        </div>
+    </div>
+    <p class="text-light">
+        <b><?php echo $_SESSION['name']; ?></b>
+        <span class="text-light"><a href="logout.php" class="btn btn-sm bg-light">LOGOUT</a></span>
+    </p>
+
+</nav>
